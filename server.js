a3dff768dbb68ea42f7346ebceec03755e99fe62
flexadm <fhuet@2xcel-us.com>
2017-02-02 13:27:19 -0800

    Refactored API with underscore

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var _ = require('underscore');
+
 var app = express();
 var PORT = process.env.PORT || 3000;
 
@@ -19,13 +21,8 @@ app.get('/todos', function(req, res){
 
 // GET /todos/:id
 app.get('/todos/:id', function(req, res) {
-    var todoId = req.params.id,
-        matchedTodo;
-    todos.forEach(function(element){
-        if (todoId === element.id.toString()) {
-            matchedTodo = element;
-        }
-    });
+    var todoId = parseInt(req.params.id, 10),
+        matchedTodo = _.findWhere(todos, {id: todoId});
     
     if (matchedTodo) {
         res.json(matchedTodo);
@@ -36,8 +33,14 @@ app.get('/todos/:id', function(req, res) {
 
 //POST /todos
 app.post('/todos', function(req, res){
-    var body = req.body;
+    var body = _.pick(req.body, 'description', 'completed');
+
+    if (!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0) {
+        return res.status(400).send();
+    }
+
     body.id = todoNextId++;
+    body.description = body.description.trim();
     todos.push(body);
     res.json(body);
 });
