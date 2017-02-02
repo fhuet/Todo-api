a05cd8267e1c68262070294e7097df4ce4582dd3
flexadm <fhuet@2xcel-us.com>
2017-02-01 19:00:43 -0800

    cleanup and setup eslint

--- server.js
+++ server.js
@@ -30,17 +30,7 @@ app.get('/todos', function(req, res){
 // GET /todos/:id
 app.get('/todos/:id', function(req, res) {
     var todoId = req.params.id,
-        matchedTodo,
-        i = 0;
-    // iterate over todo array and find the match
-    // while (i < todos.length && typeof matchedTodo === 'undefined' && typeof todoId !== 'undefined')
-    //     {if (todos[i].id.toString() === todoId)
-    //         {
-    //             matchedTodo = todos[i];
-    //         } else {
-    //             i++;
-    //         } 
-    //     };
+        matchedTodo;
     todos.forEach(function(element){
         if (todoId === element.id.toString()) {
             matchedTodo = element;
@@ -52,10 +42,6 @@ app.get('/todos/:id', function(req, res) {
     } else {
         res.status(404).send();
     }
-
-
-    // // res.status(404).send();
-    // res.send('Asking for todo with id of ' + req.params.id);
 });
 
 app.listen(PORT, function() {
