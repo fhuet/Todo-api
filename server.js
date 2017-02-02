4cf7f184794551f408139b2291a3e2e841deadef
flexadm <fhuet@2xcel-us.com>
2017-02-02 11:34:10 -0800

    "Add POST for todos

--- server.js
+++ server.js
@@ -1,22 +1,12 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 var app = express();
 var PORT = process.env.PORT || 3000;
 
-var todos = [{
-    id: 1,
-    description: "Phone Lobstie",
-    completed: false
-},
-{
-    id: 2,
-    description: "Pay rent",
-    completed: false
-},
-{
-    id: 3,
-    description: "Update Heroku",
-    completed: true
-}];
+var todos = [];
+var todoNextId = 1;
+
+app.use(bodyParser.json());
 
 app.get('/', function(req, res) {
     res.send('Todo API root');
@@ -42,6 +32,14 @@ app.get('/todos/:id', function(req, res) {
     } else {
         res.status(404).send();
     }
+});
+
+//POST /todos
+app.post('/todos', function(req, res){
+    var body = req.body;
+    body.id = todoNextId++;
+    todos.push(body);
+    res.json(body);
 });
 
 app.listen(PORT, function() {
