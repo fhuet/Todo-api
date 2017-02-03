64c9776bb41f51d2e9ff658c46c5287614fd99f2
flexadm <fhuet@2xcel-us.com>
2017-02-02 18:06:03 -0800

    Added Update todo API

--- server.js
+++ server.js
@@ -58,6 +58,32 @@ app.delete('/todos/:id', function(req, res) {
     }
 });
 
+//PUT /todos/:id
+app.put('/todos/:id', function(req, res){
+    var body = _.pick(req.body, 'description', 'completed');
+    var todoId = parseInt(req.params.id, 10),
+        matchedTodo = _.findWhere(todos, {id: todoId});
+    var validAttributes = {};
+
+    if (!matchedTodo) {
+        res.status(404).send();
+    }
+
+    if (body.hasOwnProperty('completed') && _.isBoolean(body.completed)) {
+        validAttributes.completed = body.completed;
+    } else if (body.hasOwnProperty('completed')) {
+        return res.status(400).send();
+    }
+
+    if (body.hasOwnProperty('descrition') && _.isString(body.description) && body.description.trim().length > 0) {
+        validAttributes.description = body.description.trim();
+    } else if (body.hasOwnProperty('description')) {
+        return res.status(400).send();
+    }
+
+    _.extend(matchedTodo, validAttributes);
+});
+
 
 app.listen(PORT, function() {
     console.log('Express listening on port: ' + PORT);
---
+++ variable-playground.js
@@ -0,0 +1,29 @@
+// var person = {
+//     name: 'Andrew',
+//     age: 21
+// };
+
+// function updatePerson (obj) {
+//     // obj = { 
+//     //     name: 'Andrew',
+//     //     age: 24
+//     // };
+//     obj.age = 24;
+// }
+
+// updatePerson(person);
+// console.log(person);
+
+// Array Example
+
+var grades = [
+    15, 37
+];
+
+function updateGrades (arr) {
+    // arr = [15,37,23];
+    arr.push(23);
+}
+
+updateGrades(grades);
+console.log(grades);
